423e429f4f39949ac30b6b1fceae897dea037d9e
Aliny <alinymelquiadesdesouza@gmail.com>
2024-06-24 15:19:05 -0300

    Initial commit

---
+++ src/main/java/pacote/Database.java
@@ -0,0 +1,11 @@
+package pacote;
+
+    public class Database {
+
+        private static String url = "jdbc:postgresql://localhost:5432/postgres";
+        private static String user = "postgres";
+        private static String password = "prostgres";
+
+    }
+
+
